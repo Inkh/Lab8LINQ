780c16e331dce6a9864d38eda74af0b44a083476
inkh <jhwe92@gmail.com>
2018-10-21 18:34:03 -0700

    Finished LINQ queries

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -15,6 +15,9 @@ namespace LINQ
             ConvertJSON();
         }
 
+        /// <summary>
+        /// Reads JSON file and converts it
+        /// </summary>
         static void ConvertJSON()
         {
             string path = "../../../data.json";
@@ -27,29 +30,45 @@ namespace LINQ
 
             RootObject rootObj = JsonConvert.DeserializeObject<RootObject>(data);
 
-            //foreach (var item in rootObj.features)
-            //{
-            //    Console.WriteLine(item.properties.zip);
-            //    Console.WriteLine(item.properties.state);
-            //    Console.WriteLine(item.properties.city);
-            //    Console.WriteLine(item.properties.borough);
-            //    Console.WriteLine(item.properties.county);
-            //    Console.WriteLine(item.properties.address);
-            //    Console.WriteLine(item.properties.neighborhood);
-            //}
+            // Neighborhood query with lambda
+            Console.WriteLine("Neighborhood query with Lambda");
+            var lambdaNeigh = rootObj.features.Where(x => x.properties.neighborhood != "");
 
-            //var neighborhoods = rootObj.features.Where(x => x.properties.neighborhood != "");
+            foreach (var item in lambdaNeigh)
+            {
+                Console.WriteLine(item.properties.neighborhood);
+            }
 
-            //foreach (var item in neighborhoods)
-            //{
-            //    Console.WriteLine(item.properties.neighborhood);
-            //}
-
-            var uniqueNeighborhoods = (from addr in rootObj.features
+            Console.WriteLine("\n");
+            Console.WriteLine("Non-unique neighborhoods");
+            //Non unique neighborhoods
+            var neighborhoods = (from addr in rootObj.features
                                       where addr.properties.neighborhood != ""
-                                      select addr.properties.neighborhood).Distinct();
+                                      select addr.properties.neighborhood);
+
+            //Prints all neighborhoods
+            foreach (var item in neighborhoods)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Unique neighborhoods");
+            var uniqueNeighborhoods = (from uniq in neighborhoods group uniq by uniq);
 
             foreach (var item in uniqueNeighborhoods)
+            {
+                Console.WriteLine(item.Key);
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Query chain for unique neighborhoods");
+
+            var oneLineUniqueNeighborhoods = (from uniq in rootObj.features
+                                              where uniq.properties.neighborhood != ""
+                                              select uniq.properties.neighborhood).Distinct();
+
+            foreach (var item in oneLineUniqueNeighborhoods)
             {
                 Console.WriteLine(item);
             }
